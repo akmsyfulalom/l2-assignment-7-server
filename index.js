75ce34f74585110def195a065a78ccea42e69d03
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2024-03-28 19:30:25 +0600

    clone from ass 6

---
+++ index.js
@@ -0,0 +1,282 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { ObjectId } = require('mongodb');
+const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
+const fs = require('fs');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors({ origin: ['https://akmsyful-l2-ass-6.netlify.app'], credentials: true }));
+app.use(express.json());
+
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+});
+
+// Set up multer storage for file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// MongoDB Connection URL
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        // Connect to MongoDB
+        await client.connect();
+        console.log("Connected to MongoDB");
+
+        const db = client.db('assignment-6');
+        const collection = db.collection('users');
+        const suppliesCollection = db.collection('supplies');
+
+        // User Registration
+        app.post('/api/v1/register', async (req, res) => {
+            const { name, email, password } = req.body;
+            console.log("🚀 ~ app.post ~ name, email, password:", name, email, password)
+
+            // Check if email already exists
+            const existingUser = await collection.findOne({ email });
+            if (existingUser) {
+                return res.status(400).json({
+                    success: false,
+                    message: 'User already exists'
+                });
+            }
+
+            // Hash the password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Insert user into the database
+            await collection.insertOne({ name, email, password: hashedPassword });
+
+            res.status(201).json({
+                success: true,
+                message: 'User registered successfully'
+            });
+        });
+
+        // User Login
+        app.post('/api/v1/login', async (req, res) => {
+            const { email, password } = req.body;
+
+            // Find user by email
+            const user = await collection.findOne({ email });
+            if (!user) {
+                return res.status(401).json({ message: 'Invalid email or password' });
+            }
+
+            // Compare hashed password
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+            if (!isPasswordValid) {
+                return res.status(401).json({ message: 'Invalid email or password' });
+            }
+
+            // Generate JWT token
+            const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: process.env.EXPIRES_IN });
+
+            res.json({
+                success: true,
+                message: 'Login successful',
+                token
+            });
+        });
+
+
+        // ==============================================================
+        // WRITE YOUR CODE HERE
+        // ==============================================================
+
+
+
+        app.post("/api/v1/create-supply", async (req, res) => {
+            const { image, title, category, amount, description } = req.body;
+            const result = await suppliesCollection.insertOne({
+                image, title, category, amount, description
+            });
+            res.json({
+                success: true,
+                message: "Successfully supply create",
+                result
+            });
+        });
+
+      
+        app.get("/api/v1/supplies", async (req, res) => {
+            try {
+                const { limit } = req.query;
+                let result;
+
+                if (limit) {
+                    result = await suppliesCollection.find({}).limit(parseInt(limit)).toArray();
+                } else {
+                    result = await suppliesCollection.find({}).toArray();
+                }
+
+                res.json({
+                    success: true,
+                    message: "Successfully retrieve supplies!",
+                    data: result
+                });
+            } catch (error) {
+                res.status(500).json({
+                    success: false,
+                    message: "Failed to retrieve supplies",
+                    error: error.message
+                });
+            }
+        });
+
+
+        app.get("/api/v1/supply/:id", async (req, res) => {
+            try {
+                const { id } = req.params;
+                const data = await suppliesCollection.findOne(new ObjectId(id));
+
+                if (!data) {
+                    return res.status(404).json({
+                        success: false,
+                        message: "Supply not found",
+                    });
+                }
+
+                res.json({
+                    success: true,
+                    message: "Successfully retrieved supply",
+                    data,
+                });
+            } catch (error) {
+                console.error("Error retrieving supply:", error);
+                res.status(500).json({
+                    success: false,
+                    message: "Internal server error",
+                    error: error.message,
+                });
+            }
+        });
+
+
+        app.delete("/api/v1/supply/:id", async (req, res) => {
+            try {
+                const { id } = req.params; // Extract ID from request parameters
+                if (!id) {
+                    throw new Error("Invalid supply ID");
+                }
+                const data = await suppliesCollection.deleteOne({
+                    _id: new ObjectId(id),
+                });
+                if (!data) {
+                    return res.status(404).json({
+                        success: false,
+                        message: "Supply not found",
+                    });
+                }
+                res.json({
+                    success: true,
+                    message: "Successfully deleted supply!",
+                    data,
+                });
+            } catch (error) {
+                console.error("Error deleting supply:", error);
+                res.status(500).json({
+                    success: false,
+                    message: "Failed to delete supply",
+                    error: error.message,
+                });
+            }
+        });
+
+
+        app.put("/api/v1/supply/:id", async (req, res) => {
+            const { id } = req.params;
+            const { image, title, category, amount, description } = req.body;
+
+            try {
+
+                const existingSupply = await suppliesCollection.findOne({ _id: new ObjectId(id) });
+                if (!existingSupply) {
+                    return res.status(404).json({ success: false, message: "Supply not found" });
+                }
+
+
+                await suppliesCollection.updateOne(
+                    { _id: new ObjectId(id) },
+                    { $set: { image, title, category, amount, description } }
+                );
+
+                res.json({
+                    success: true,
+                    message: "Successfully updated supply",
+                });
+            } catch (error) {
+                console.error("Error updating supply:", error);
+                res.status(500).json({ success: false, message: "Internal server error" });
+            }
+        });
+
+
+
+
+
+
+
+        app.post('/api/v1/upload', upload.single('image'), async (req, res) => {
+            try {
+                // Check if file exists
+                if (!req.file) {
+                    return res.status(400).json({ success: false, message: 'No file uploaded' });
+                }
+
+                const base64String = req.file.buffer.toString('base64');
+
+
+                const result = await cloudinary.uploader.upload(`data:image/png;base64,${base64String}`, { resource_type: "auto", folder: 'weblearn' });
+
+
+                res.status(200).json({
+                    success: true,
+                    message: "Image uploaded successfully",
+                    url: result.secure_url
+                });
+            } catch (err) {
+                console.error("Error uploading image:", err);
+                res.status(500).json({
+                    success: false,
+                    message: "Error uploading image",
+                    error: err.message
+                });
+            }
+        });
+
+        // Start the server
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+
+    } finally {
+    }
+}
+
+run().catch(console.dir);
+
+// Test route
+app.get('/', (req, res) => {
+    const serverStatus = {
+        message: 'Server is running smoothly',
+        timestamp: new Date()
+    };
+    res.json(serverStatus);
+});
+
