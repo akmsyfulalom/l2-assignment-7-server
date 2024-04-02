dd7879941976aa48d65f19de59dbd9ebfe7acb75
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2024-04-03 03:40:29 +0600

    ok

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['https://dev--l2-assignment-7.netlify.app'], credentials: true }));
 app.use(express.json());
 
 // Configure Cloudinary
